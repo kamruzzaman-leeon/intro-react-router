aee210d2a84536e4fb4a4db1c6577ad96fe6a511
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-09-29 21:15:31 +0600

    navigate added

--- src/Component/PostDetails/PostDetails.jsx
+++ src/Component/PostDetails/PostDetails.jsx
@@ -1,14 +1,19 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const PostDetails = () => {
     const post = useLoaderData();
+    const navigate = useNavigate();
+    const handleGoBack = () =>{
+        navigate(-1);
+    }
     const {id, title, body}= post;
     return (
         <div>
             <h2>Post ID: {id}</h2>
             <h3>Title: {title}</h3>
             <p><small>{body}</small></p>
+            <button className="btn btn-warning" onClick={handleGoBack}>Go back</button>
         </div>
     );
 };
